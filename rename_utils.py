c883e35f84174ad20731b159f4a0b4da09ade319
ofelipegoulart <ofelipegoulart@gmail.com>
2022-09-03 13:59:49 -0300

    Novos métodos em rename_utils.py

--- rename_utils.py
+++ rename_utils.py
@@ -21,6 +21,12 @@ class RENAME2022:
         name_csv_file = self.csv_file[0:-4]
         pd_treated = pd.read_csv('./planilhas_apendice/' + csv_file)
         new_pd = pd_treated.fillna(method='ffill')
+        '''
+        Importante: o método ffill preenche as células vazias com o valor anterior em todas as colunas 
+        do dataframe. Tome cuidado para não preencher valores vazios em células que no RENAME não possuem
+        valor no documento original.
+        Se utilizar, modifique para a coluna do seu interesso, de modo a reduzir retrabalhos.
+        '''
         new_pd.to_csv(f'{name_csv_file}_atualizado.csv')
 
     def generate_final_file(self):
@@ -38,12 +44,27 @@ class RENAME2022:
             df_sheets.insert(5, name_new_column, '')
             df_sheets.columns = main_columns
             list_dataframes.append(df_sheets)
-        list_dataframes.insert(6,pd.read_csv('./planilhas_apendice/apendice_a_j.csv'))
+        list_dataframes.insert(6, pd.read_csv('./planilhas_apendice/apendice_a_j.csv'))
         final_result = pd.concat(list_dataframes)
         final_result.to_csv('./planilhas_apendice_resultado/resultado.csv')
+
+    @staticmethod
+    def length_column():
+        """
+        Função para descobrir quantos caracteres possui a maior
+        ‘string’ da coluna de Concentração/Composição. Será importante
+        para a criação do arquivo SQL.
+        """
+        df = pd.read_csv('planilhas_apendice_resultado/resultado.csv')
+        column = df['Concentração/\nComposição']
+        return max(column.str.len())
+
+    @staticmethod
+    def get_name_columns():
+        df = pd.read_csv('planilhas_apendice_resultado/resultado.csv')
+        return list(df.columns)
 
 
 if __name__ == '__main__':
     r = RENAME2022('planilhas_apendice')
-    r.generate_final_file()
-
+    print(r.get_name_columns())
