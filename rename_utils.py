c5f169692600e4414864c3adc68ab55c4358a6ec
ofelipegoulart <ofelipegoulart@gmail.com>
2022-09-05 13:15:38 -0300

    Correção arquivo csv

--- rename_utils.py
+++ rename_utils.py
@@ -7,27 +7,19 @@ class RENAME2022:
     def __init__(self, csv_folder):
         self.csv_folder = csv_folder
         self.csv_file = None
-
-    def concatenate_tables(self):
-        list_dataframes = list()
-        for filename in glob.glob(self.csv_folder + '/*.csv'):
-            table = pd.read_csv(filename)
-            list_dataframes.append(table)
-            final_dataframe = pd.concat(list_dataframes)
-            final_dataframe.to_csv('lista_rename_2022.csv')
+        self.list_dataframes = None
 
     def fill_name_medicine(self, csv_file):
         self.csv_file = csv_file
         name_csv_file = self.csv_file[0:-4]
-        pd_treated = pd.read_csv('./planilhas_apendice/' + csv_file)
-        new_pd = pd_treated.fillna(method='ffill')
-        '''
-        Importante: o método ffill preenche as células vazias com o valor anterior em todas as colunas 
-        do dataframe. Tome cuidado para não preencher valores vazios em células que no RENAME não possuem
-        valor no documento original.
-        Se utilizar, modifique para a coluna do seu interesso, de modo a reduzir retrabalhos.
-        '''
+        pd_treated = pd.read_csv('./planilhas_apendice_resultado/' + csv_file)
+        new_pd = pd_treated['Denominação genérica'].fillna(method='ffill')
         new_pd.to_csv(f'{name_csv_file}_atualizado.csv')
+
+    @staticmethod
+    def concatenate_tables(list_dataframes):
+        final_result = pd.concat(list_dataframes)
+        final_result.to_csv('./planilhas_apendice_resultado/resultado.csv')
 
     def generate_final_file(self):
         list_files = list()
@@ -38,15 +30,15 @@ class RENAME2022:
         list_files.remove('apendice_a_j.csv')
         for sheets in list_files:
             main_df = pd.read_csv('./planilhas_apendice/apendice_a_j.csv')
+            # Inserindo a coluna Classificação AWaRe em todos os dataframes
             main_columns = main_df.columns
             df_sheets = pd.read_csv(f'./planilhas_apendice/{sheets}')
             name_new_column = main_columns[-1]
-            df_sheets.insert(5, name_new_column, '')
+            df_sheets.insert(5, name_new_column, ' ')
             df_sheets.columns = main_columns
             list_dataframes.append(df_sheets)
         list_dataframes.insert(6, pd.read_csv('./planilhas_apendice/apendice_a_j.csv'))
-        final_result = pd.concat(list_dataframes)
-        final_result.to_csv('./planilhas_apendice_resultado/resultado.csv')
+        self.concatenate_tables(list_dataframes)
 
     @staticmethod
     def length_column():
@@ -56,8 +48,14 @@ class RENAME2022:
         para a criação do arquivo SQL.
         """
         df = pd.read_csv('planilhas_apendice_resultado/resultado.csv')
-        column = df['Concentração/\nComposição']
-        return max(column.str.len())
+        list_columns_name = []
+        for i in range(1, 7):
+            column_name = df.columns[i]
+            info_columns = {
+                column_name: max(df[column_name].str.len())
+            }
+            list_columns_name.append(info_columns)
+        return list_columns_name
 
     @staticmethod
     def get_name_columns():
@@ -67,4 +65,6 @@ class RENAME2022:
 
 if __name__ == '__main__':
     r = RENAME2022('planilhas_apendice')
-    print(r.get_name_columns())
+    r.generate_final_file()
+    print(r.length_column())
+    # r.fill_name_medicine('resultado.csv')
